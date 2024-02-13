34b572edf58e68d1589c0257364f48171642b197
Aditya G <158847652+AdityaG252@users.noreply.github.com>
2024-02-13 15:43:45 +0530

    Update and rename Covid19 Project.sql to Covid19 Portfolio Project.sql

--- Covid19 Portfolio Project.sql
+++ Covid19 Portfolio Project.sql
@@ -1,46 +1,63 @@
+/*
+Covid 19 Data Exploration 
+
+Skills used: Joins, CTE's, Temp Tables, Windows Functions, Aggregate Functions, Creating Views, Converting Data Types
+
+*/
+
 Select*
 From [Covid]..[CovidDeaths$]
+Where continent is not null 
 order by 3,4;
 
---Select*
---From [dbo].[CovidVaccinations$]
---order by 3,4
 
---Data we are going to use
+-- Select Data that we are going to be starting with
 
 Select Location,Date,total_cases,new_cases,total_deaths,population
 From [Covid]..[CovidDeaths$]
+Where continent is not null 
 order by 1,2;
 
--- Death Percentage (total caces vs total deaths)
+
+-- Total Cases vs Total Deaths
+-- Shows likelihood of dying if you contract covid in your country
 
 Select Location,Date,total_cases,total_deaths,(total_deaths/total_cases)*100 as DeathPercentage
 From [Covid]..[CovidDeaths$]
+Where location like 'India'
+and continent is not null 
 order by 1,2;
 
---(total caces vs population)
+
+-- Total Cases vs Population
+-- Shows what percentage of population infected with Covid
 
 Select Location,Date,population,total_cases,(total_cases/population)*100 as PercentofPopulaionInfected
 From [Covid]..[CovidDeaths$]
 --where location ='India'
 order by 1,2;
 
---Looking for countries with highest infection rates compared to population
+
+-- Countries with Highest Infection Rate compared to Population
 
 Select Location,population,max(total_cases) as HighestInfectionRate,Max((total_cases/population))*100 as HighestPercentofPopulaionInfected
 From [Covid]..[CovidDeaths$]
 Group by location,population
 order by HighestPercentofPopulaionInfected desc
+  
 
---Looking for countries with highest death counts 
+-- Countries with Highest Death Count per Population
 
 Select Location,max(Cast(total_deaths as int)) as totalDeathCount
 From [Covid]..[CovidDeaths$]
 where continent is not null
 Group by location
 order by totalDeathCount desc
+  
 
---Looking for continents with highest death counts
+-- BREAKING THINGS DOWN BY CONTINENT
+
+-- Showing contintents with the highest death count per population
 
 Select continent,max(Cast(total_deaths as int)) as totalDeathCount
 From [Covid]..[CovidDeaths$]
@@ -48,13 +65,8 @@ where continent is not null
 Group by continent
 order by totalDeathCount desc
 
+  
 --Global numbers
-
-Select date,sum(new_cases) as totalCases, Sum(cast(new_deaths as int))as totaldeaths,Sum(cast(new_deaths as int))/sum(new_cases) *100 as GlobalDeathPercentage
-From [Covid]..[CovidDeaths$]
-where continent is not null
-Group by date
-order by 1,2
 
 Select sum(new_cases) as totalCases, Sum(cast(new_deaths as int))as totaldeaths,Sum(cast(new_deaths as int))/sum(new_cases) *100 as GlobalDeathPercentage
 From [Covid]..[CovidDeaths$]
@@ -62,17 +74,21 @@ where continent is not null
 --Group by date
 order by 1,2
 
---Looking at total population vs vaccination
+  
+-- Total Population vs Vaccinations
+-- Shows Percentage of Population that has recieved at least one Covid Vaccine
 
 Select dea.continent, dea.location, dea.date, dea.population, vac.new_vaccinations,
 Sum(Convert(int,vac.new_vaccinations)) over (Partition by dea.location order by dea.location, dea.date) as Roolingpeoplevaccinated 
 From Covid..CovidDeaths$ dea
 join Covid..CovidVaccinations$ vac
-on dea.location=vac.location and dea.date=vac.date
+    on dea.location=vac.location 
+    and dea.date=vac.date
 where dea.continent is not null
 order by 2,3
 
--- Using CTE
+  
+-- Using CTE to perform Calculation on Partition By in previous query
 
 With PopVsVac (Continent, location, date, population, new_vaccination, Roolingpeoplevaccinated)
 as
@@ -81,14 +97,16 @@ Select dea.continent, dea.location, dea.date, dea.population, vac.new_vaccinatio
 Sum(Convert(int,vac.new_vaccinations)) over (Partition by dea.location order by dea.location, dea.date) as Roolingpeoplevaccinated 
 From Covid..CovidDeaths$ dea
 join Covid..CovidVaccinations$ vac
-on dea.location=vac.location and dea.date=vac.date
+      on dea.location=vac.location 
+      and dea.date=vac.date
 where dea.continent is not null
 --order by 2,3
 )
 select *, (Roolingpeoplevaccinated/population)*100
 from PopVsVac
 
---temp table
+
+-- Using Temp Table to perform Calculation on Partition By in previous query
 
 Create table #PerPopulationVaccinated
 (
@@ -104,21 +122,23 @@ Select dea.continent, dea.location, dea.date, dea.population, vac.new_vaccinatio
 Sum(Convert(int,vac.new_vaccinations)) over (Partition by dea.location order by dea.location, dea.date) as Roolingpeoplevaccinated 
 From Covid..CovidDeaths$ dea
 join Covid..CovidVaccinations$ vac
-on dea.location=vac.location and dea.date=vac.date
+      on dea.location=vac.location 
+      and dea.date=vac.date
 where dea.continent is not null
 --order by 2,3
 select *, (Roolingpeoplevaccinated/population)*100
 from #PerPopulationVaccinated
 
 
---Creating view to store data for later vizulization
+-- Creating View to store data for later visualizations
 
 create view PerPopulationVaccinated as
 Select dea.continent, dea.location, dea.date, dea.population, vac.new_vaccinations,
 Sum(Convert(int,vac.new_vaccinations)) over (Partition by dea.location order by dea.location, dea.date) as Roolingpeoplevaccinated 
 From Covid..CovidDeaths$ dea
 join Covid..CovidVaccinations$ vac
-on dea.location=vac.location and dea.date=vac.date
+      on dea.location=vac.location 
+      and dea.date=vac.date
 where dea.continent is not null
 --order by 2,3
 
